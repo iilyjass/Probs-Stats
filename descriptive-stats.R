618df4a8c8614fbb57363edb62acb521060bb310
iilyjass <jasyoung@claflin.edu>
2025-09-02 15:46:55 -0400

    Add files via upload

---
+++ descriptive-stats.R
@@ -0,0 +1,14 @@
+#Author: Jasmine; Data:Aug 28,2025; Purpose: Measure of descriptive stats
+
+#Generate dummy/sample dataset
+data <- c(1:50)
+
+#Calculate mean/average for data
+mean(data)
+
+#Lets calculate median for data
+median(data)
+
+#calculate standard deviation in data
+sd(data)
+
